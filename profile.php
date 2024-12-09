a0cd6a908f54a87920cb7f75b4cb8107c0c558e5
kudosVV <157321942+kudosVV@users.noreply.github.com>
2024-12-08 21:17:28 -0600

    Add files via upload

--- profile.php
+++ profile.php
@@ -1,254 +1,256 @@
 <?php
 
-include_once 'config.php';
+if ((isset($_GET['memberID'])) && (is_numeric($_GET['memberID'])) ) {
 
+    $memberID = $_GET['memberID'];
+} elseif ((isset($_POST['memberID'])) && (is_numeric($_POST['memberID'])) ) {
+    $memberID = $_POST['memberID'];
+} else {
+    $pageContent .=  '<p class="error">No member ID has been passed.</p>';
+    exit();
+}
+
+require 'config.php';
+$pk = $m = $i = $mx = false;
 $valid = false;
+
+if ($pk || $m) {
+    $banner = '<div class="alert alert-success" role="alert">
+    Success! Record Updated!
+  </div>';
+} elseif ($mx || $i ){
+    $banner = '<div class="alert alert-success" role="alert">
+    Success! Record Updated!
+  </div>';
+}
+if (isset($_POST['update'])) {
 $insert_success= false;
-$firstname = $lastname = $email = $pageContent = NULL;
-
-$invalid_fname = $invalid_lname = $invalid_email = $invalid_email_format = $invalid_image = $invalid_password = NULL;
+$invalid_fname = $invalid_password = $invalid_lname = $invalid_email = $invalid_image = NULL;
 $errors = [];
-
-if (isset($_POST['submit'])) {
-    $valid = true;
-    if (empty($_POST['firstname'])) {
-        $invalid_fname = '<div class="alert alert-danger">Please enter a valid first name</div>';
-        $errors = 'error';
-        $valid = false;
-    } else {
-        $firstname = ucfirst(htmlspecialchars(trim($_POST['firstname'])));
-        $firstname = mysqli_real_escape_string($conn, trim($_POST['firstname']));
-       
+if (empty($_POST['firstname'])) {
+    $invalid_fname = '<div class="alert alert-danger">Please enter a valid first name</div>';
+    $errors[] = 'no first name';
+    
+    $valid = false;
+}else {
+$firstname = ucfirst(htmlspecialchars(trim($_POST['firstname'])));
+$firstname = mysqli_real_escape_string($conn, trim($_POST['firstname']));
     }
-    if (empty($_POST['lastname'])) {
-        $invalid_lname = '<div class="alert alert-danger">Please enter a valid last name</div>';
-        $errors = 'error';
-        $valid = false;
-    } else {
-        $lastname = ucfirst(htmlspecialchars(trim($_POST['lastname'])));
-        $lastname = mysqli_real_escape_string($conn, trim($_POST['lastname']));
-    }
-    if (empty($_POST['email'])) {
-        $invalid_email = '<div class="alert alert-danger">Please enter a valid email</div>';
-        $errors = 'error';
-        $valid = false;
-    } else {
-        $email = trim($_POST['email']);
-        $email = mysqli_real_escape_string($conn, trim($_POST['email']));
-        // validate email using a regular expression
-        if (!preg_match('/[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/', $email)) {
-            // returns 1 (true) for match, 0 (false) for no match
-            $invalid_email_format = '<div class="alert alert-danger">Invalid email format</div>';
-            $errors = 'error';
-            $valid = false;
-        }
-    }
-    if (empty($_POST['password'])) {
-        $invalid_password = '<div class="alert alert-danger">Please enter a password</div>';
-        $errors = 'error';
-        $valid = false;
-
-    } else {
-        $password = trim($_POST['password']);
-    }
-    if (empty($_POST['verifypassword'])) {
-        $invalid_password_verify = '<div class="alert alert-danger">Please verify your password</div>';
-        $errors = 'error';
-    }
-    else {
-        
-         $passwordVerify = trim($_POST['verifypassword']);
-    }
-      if(strcmp($_POST['password'], $_POST['verifypassword']) == 0) {  
-            $password = $_POST['password']; 
-            $password = mysqli_real_escape_string($conn, trim($_POST['password'])); 
-           $valid = true;
-        } else {  
-            $invalid_password_verify = '<div class="alert alert-danger">password mismatch!!</div>';  
-			$valid = false;
-            $errors = 'error';
-        }  
+if (empty($_POST['lastname'])) {
+    $invalid_lname = '<div class="alert alert-danger">Please enter a valid last name</div>';
+    $errors[] = 'error last name';
+    $valid = false;
+} else {
+    $lastname = ucfirst(htmlspecialchars(trim($_POST['lastname'])));
+    $lastname = mysqli_real_escape_string($conn, trim($_POST['lastname']));   
+}
 $fullname = $firstname . " " . $lastname;
 $username = strtolower(substr($firstname, 0, 1) . $lastname);
 $username = mysqli_real_escape_string($conn, trim($username));
-$query1="SELECT username FROM `membership` WHERE username='$username'";
-$resulted = mysqli_query($conn, $query1);
-$ranything_found = mysqli_num_rows($resulted);
-if($ranything_found>0){
-	$invalid_lname = '<div class="alert alert-danger">Username is already taken.</div>';
-	$valid = false;
+$valid = true;
+    if ($valid) {
+        $sql = "UPDATE `membership` SET `firstname`='$firstname',`lastname`='$lastname', `username`='$username' WHERE `memberID`=$memberID LIMIT 1";
+        $r = @mysqli_query($conn, $sql);
+        if (mysqli_affected_rows($conn) == 1) {
+                 $pk = true; 
+                 $lastname = '<div class="alert alert-success">Name has been updated!</div>';
+             }
+             else {
+                $pk = false;
+             }
+if (empty($_POST['email'])) {
+    $invalid_email = '<div class="alert alert-danger">Please enter a valid email</div>';
+    $errors[] = 'error email';
+    $valid = false;
 } else {
-	$valid=true;
-}
-$filetype = pathinfo($_FILES['profilePic']['name'],PATHINFO_EXTENSION);
-if ((($filetype == "gif") or ($filetype == "jpg") or ($filetype == "png")) and $_FILES['profilePic']['size'] < 100000) {
-	// check to make sure there is no error on the upload. If so, display the errror 
-	if ($_FILES["profilePic"]["error"] > 0) {
-		$invalid_image .= '<div class="alert alert-danger">' . "Return Code: " . $_FILES["profilePic"]["error"] . "<br>" . '</div>';
-        $errors = 'error';
-	} else {
-		// display information about the file 
-		// = "Upload: " . $_FILES["profilePic"]["name"] . "<br>";
-		//$pageContent .= "Type: " . $_FILES["profilePic"]["type"] . "<br>";
-		//$pageContent .=  "Size: " . ($_FILES["profilePic"]["size"] / 1024) . " Kb<br>";
-		//$pageContent .=  "Temp file: " . $_FILES["profilePic"]["tmp_name"] . "<br>";
-		// if the file already exists in the upload directory, give an error
-		if (file_exists("upload/" . $_FILES["profilePic"]["name"])) {
-			$invalid_image .= '<div class="alert alert-danger">' . $_FILES["profilePic"]["name"] . " already exists. " . '</div>';
-            $valid = false;
-            $insert_success = false;
-            $errors = 'error'; 
-		} else {
-			// move the file to a permanent location
-			move_uploaded_file($_FILES["profilePic"]["tmp_name"],"upload/" . $_FILES["profilePic"]["name"]);
-            $invalid_image .= "Stored in " . "upload/" . $FILES["profilePic"]["name"];
-			$filename = "membership.txt";
-            $valid=true;
-// format the form data to be saved to the txt file
-        $data_entry = $fullname . "," . $email . "," . $password . "," . $username . "\n";
-// open the txt file ($filename) to append (a) and assign it to a file handle ($fp)
-        $fp = fopen($filename, "a") or die ("Couldn't open file, sorry.");
-// write (fwrite) the data ($data_entry) to the file using the file handle ($fp)
-        if (fwrite($fp, $data_entry) > 0) { // successful write operation returns 1, failure returns 0
-	// do this on success
-	    $logged_in = TRUE; // sets a login trigger for the program to switch from form display to content display
-    } else {
-	// do this on failure
-	     $errors .= "Your information was not saved. Please try again at another time."; 
-    }
-        $fp = fclose($fp); // close the file
-        $imagePath = "upload/" . $_FILES["profilePic"]["name"];
-	 }
-	}
-} else {
-	    $invalid_image .= '<div class="alert alert-danger">Invalid file</div>';
+    $email = trim($_POST['email']);
+    $email = mysqli_real_escape_string($conn, trim($_POST['email']));
+    // validate email using a regular expression
+    if (!preg_match('/[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/', $email)) {
+        // returns 1 (true) for match, 0 (false) for no match
+        $invalid_email_format = '<div class="alert alert-danger">Invalid email format</div>';
+        $errors[] = 'error invalid email';
         $valid = false;
     }
-    if (!$conn) {
-		echo "Failed to connect to MySQL: ".mysqli_connect_error($conn);
-	}
-    if ($valid) {
+}
 
-	$query = "INSERT INTO `membership` VALUES (DEFAULT,'$firstname','$lastname','$username','$email','$password', '$imagePath');";
-	$result2 = mysqli_query($conn, $query) or die(mysqli_error($conn));
-	if (!$result2) {
-		die(mysqli_error($conn));
-	} else {
-		$row_count = mysqli_affected_rows($conn);
-		if ($row_count == 1) {
-			// retrieve the last record inserted id
-			$memberID = mysqli_insert_id($conn);
-			$insert_success = TRUE;
-			echo '<div class="alert alert-success"><strong>Success! </strong>Record inserted!!</div>';
+if ($valid) {
+    $sql = "UPDATE `membership` SET `email`='$email' WHERE `memberID`=$memberID LIMIT 1";
+    $r = @mysqli_query($conn, $sql);
+    if (mysqli_affected_rows($conn) == 1) {
+             $mx = true;
+             $invalid_email = '<div class="alert alert-success">Email has been updated!!</div>';
+             
+         }  else {
+            $mx = false;
+         }
 
-		} else {
-            $valid = false;
-			echo '<div class="alert alert-danger">record failed!!</div>';
-           
-			}
-		}
-	}
-}  
-if ($insert_success) {
+}
+
+if (empty($_POST['password'])) {
+    $invalid_password = '<div class="alert alert-danger">Please enter a password</div>';
+    $errors[] = 'error password';
+    $valid = false;
+
+} else {
+    $password = trim($_POST['password']);
+}
+if (empty($_POST['verifypassword'])) {
+    $invalid_password_verify = '<div class="alert alert-danger">Please verify your password</div>';
+    $errors[] = 'error';
+    $valid=false;
+}
+else {
+     $passwordVerify = trim($_POST['verifypassword']);
+}
+     if(strcmp($_POST['password'], $_POST['verifypassword']) == 0) {  
+        $password = $_POST['password']; 
+        $password = mysqli_real_escape_string($conn, trim($_POST['password']));  
+    } else {  
+        $invalid_password_verify = '<div class="alert alert-danger">password mismatch!!</div>';  
+        $valid = false;
+        $errors[] = 'error';
+    }  
+if ($valid) {
+    $sql = "UPDATE `membership` SET `password`='$password' WHERE `memberID`=$memberID LIMIT 1";
+    $r = @mysqli_query($conn, $sql);
+    if (mysqli_affected_rows($conn) == 1) {
+             $m = true;
+             $invalid_password = '<div class="alert alert-success">Password Updated!</div>';
+             
+         }  else {
+            $m = false;
+         }
+
+}
+
+$filetype = pathinfo($_FILES['profilePic']['name'],PATHINFO_EXTENSION);
+if ((($filetype == "gif") or ($filetype == "jpg") or ($filetype == "png")) and $_FILES['profilePic']['size'] < 100000) {
+// check to make sure there is no error on the upload. If so, display the errror 
+if ($_FILES["profilePic"]["error"] > 0) {
+    $invalid_image .= '<div class="alert alert-danger">' . "Return Code: " . $_FILES["profilePic"]["error"] . "<br>" . '</div>';
+    $errors[] = 'error';
+    $valid=false;
+} else {
+    // display information about the file 
+    // = "Upload: " . $_FILES["profilePic"]["name"] . "<br>";
+    //$pageContent .= "Type: " . $_FILES["profilePic"]["type"] . "<br>";
+    //$pageContent .=  "Size: " . ($_FILES["profilePic"]["size"] / 1024) . " Kb<br>";
+    //$pageContent .=  "Temp file: " . $_FILES["profilePic"]["tmp_name"] . "<br>";
+    // if the file already exists in the upload directory, give an error
+    
+        // move the file to a permanent location
+        move_uploaded_file($_FILES["profilePic"]["tmp_name"],"upload/" . $_FILES["profilePic"]["name"]);
+        $invalid_image .= "Stored in " . "upload/" . $FILES["profilePic"]["name"];
+        $valid = true;
+        $filename = "membership.txt";
+// format the form data to be saved to the txt file
+    $data_entry = $fullname . "," . $email . "," . $password . "," . $username . "\n";
+// open the txt file ($filename) to append (a) and assign it to a file handle ($fp)
+    $fp = fopen($filename, "a") or die ("Couldn't open file, sorry.");
+// write (fwrite) the data ($data_entry) to the file using the file handle ($fp)
+    if (fwrite($fp, $data_entry) > 0) { // successful write operation returns 1, failure returns 0
+// do this on success
+    $logged_in = TRUE; // sets a login trigger for the program to switch from form display to content display
+} else {
+// do this on failure
+     $pageContent .= "Your information was not saved. Please try again at another time."; 
+     $errors[] = 'information not saved';
+}
+    $fp = fclose($fp); // close the file
+    $imagePath = "upload/" . $_FILES["profilePic"]["name"];
+    $image =  $_FILES["profilePic"]["name"];
+    $valid = true;
+    $image1 = true;
+ }
+
+} 
+} else {
+   
+    $valid = false;
+}
+if (empty('profilePic')) {
+    $invalid_image = '<div class="alert alert-danger">Please enter an image</div>';
+}
+if (!$conn) {
+    echo "Failed to connect to MySQL: ".mysqli_connect_error($conn);
+}
+if (isset($image)) {
+
+    $sql = "UPDATE `membership` SET `image`='$image' WHERE `memberID`=$memberID LIMIT 1";
+       $r = @mysqli_query($conn, $sql);
+       if (mysqli_affected_rows($conn) == 1) {
+                $i = true;
+                $pageContent = '<div class="alert alert-success">Image updated!!</div>';
+                
+            } else {
+                $i = false;
+                }
+            }
+        } 
+    
+    
+
+
     $query = "SELECT * FROM `membership` WHERE `memberID` = $memberID;";
     $result = mysqli_query($conn,$query);
-    if (!$result) {
-        die(mysqli_error($conn));
-    }
+    
     if ($row = mysqli_fetch_assoc($result)) {
-        // set the database field values to local variables for futher use in the script
-        $memberID = $row['memberID'];
+    // set the database field values to local variables for futher use in the script
         $firstname = $row['firstname'];
         $lastname = $row['lastname'];
         $username = $row['username'];
         $email = $row['email'];
         $password = $row['password'];
         $image = $row['image'];
-    $pageContent = <<<HERE
-    <table style="width:100%" class="table">
-    <tr style="background-color: #D6EEEE" class="table-primary">
-        <th>
-        </th>
-        <th>Membership ID</th>
-        <th>first name</th>
-        <th>last name</th>
-        <th>username</th>
-        <th>email</th>
-        <th>password</th>
-        <th>image</th>
-    </tr><br><br>
-    <tr class="table-secondary">
-        <th>
-        <a href="delete-verify.php?memberID=$memberID">
-        <input type="button" name="delete"  value="Delete" class="btn btn-primary btn-md center-block method="post">
-        </a>
-
-         <a href="update.php?memberID=$memberID">
-        <input type="button" name="update"  value="Update" class="btn btn-primary btn-md center-block" method="post">
-        </a>
-
-
-        
-        </th>
-        <th>$memberID</th>
-        <th>$firstname</th>
-        <th>$lastname</th>
-        <th>$username</th>
-        <th>$email</th>
-        <th>$password</th>
-        <th>$image</th>
-    </tr>
-    </table>
-    HERE;
     }
-    } else {
-$pageContent = <<<HERE
-<section class="container">
-	<p>Please register if you are a new member</p>
+
+    if (mysqli_num_rows($result) == 1) {
+     $row = mysqli_fetch_array($result, MYSQLI_NUM);
+$pageContent .= 
+'<section class="container">
+	<p>Update your details</p>
 	<form action="" enctype="multipart/form-data" method="post">
 		<div class="form-group">
 			<label for="firstname">First Name:</label>
-			<input type="text" name="firstname" id="firstname" value="$firstname" class="form-control"> $invalid_fname
+			<input type="text" name="firstname" id="firstname" value="' . $firstname .'" class="form-control">' . $invalid_fname .'
 		</div>
 		<div class="form-group">
 			<label for="lastname">Last Name:</label>
-			<input type="text" name="lastname" id="lastname" value="$lastname" class="form-control"> $invalid_lname
+			<input type="text" name="lastname" id="lastname" value="' . $lastname .'" class="form-control">' . $invalid_lname .'
 		</div>
 		<div class="form-group">
 			<label for="email">E-Mail:</label>
-			<input type="text" name="email" id="email" value="$email" class="form-control"> $invalid_email $invalid_email_format
+			<input type="text" name="email" id="email" value="' . $email .'" class="form-control">' . $invalid_email . $invalid_email_format . '
 		</div>
         <div class="form-group">
         <label for="password">Password: </label>
-        <input type="password" name="password" id="password" value ='' class="form-control">$invalid_password
+        <input type="password" name="password" id="password" value ="' . $password .'" class="form-control"> ' .$invalid_password .'
         </div>
           <div class="form-group">
         <label for="verifypassword">Verify Password: </label>
-        <input type="password" name="verifypassword" id="verifypassword" value ='' class="form-control">$invalid_password_verify $invalid_password_key
+        <input type="password" name="verifypassword" id="verifypassword" value="' . $password . '" class="form-control"> ' . $invalid_password_verify . $invalid_password_key . '
         </div>
 		<p>Please select an image for your profile.</p>
 		<div class="form-group">
-			<input type="hidden" name="MAX_FILE_SIZE" value="100000">
-			<label for="profilePic">File to Upload:</label> $invalid_image
+			<input type="hidden" name="MAX_FILE_SIZE" value="10000">
+			<label for="profilePic">File to Upload: </label>' . $invalid_image . '
 			<input type="file" name="profilePic" id="profilePic" class="form-control">
 		</div>
 		<div class="form-group">
-        
+        <input type="hidden" name="memberID" value="' . $image . '">
          <input type="hidden" name="memberID" value="' . $memberID . '">
-		<input type="submit" name="submit" value="Submit Profile" class="btn btn-primary">
-       
-        
+		<input type="submit" name="update" value="Update Profile" class="btn btn-primary">
 		</div>
 	</form>
-</section>\n
-<br><br><br>
-HERE;
-}
-session_start();
-if (isset($_SESSION['message'])) {
-    echo $_SESSION['message'];
-    unset($_SESSION['message']);
-}
+</section>
+<br><br><br>';
+    
+    }
+     
+    
+mysqli_close($conn);
 $pageTitle = "Profile page";
 include_once 'template.php';
+
 ?>
